bd455fae615127418ee42a343ceb830f3c7faa16
Ogorek <Ogorek@Ogorek-Komputer>
2017-01-10 18:42:24 +0100

    Łączenie z plikami

---
+++ src/pamiec/KartyWPamieci.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+osoba-> trudniejsza do wczytania, MYŚL, gettery. LINIJKA POD LINIJKĄ- dane
+
+ */
+package pamiec;
+
+/**
+ *
+ * @author Ogorek
+ */
+public class KartyWPamieci {
+    
+}
