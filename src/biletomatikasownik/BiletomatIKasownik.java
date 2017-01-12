05bfb9ba49c515cef859dfe115e09659fd7c20e3
Ogorek <Ogorek@Ogorek-Komputer>
2017-01-12 18:35:55 +0100

    WYDAWANIE RESZTY

--- src/biletomatikasownik/BiletomatIKasownik.java
+++ src/biletomatikasownik/BiletomatIKasownik.java
@@ -14,15 +14,15 @@ public class BiletomatIKasownik {
         new Biletomat_frame(Biletomat_frame.NAZWA_FRAME);
 
         HashMap<Double, Integer> monety = new HashMap();
-       /* monety.put(5.0, 15);
+        monety.put(5.0, 15);
         monety.put(2.0, 15);
         monety.put(1.0, 125);
         monety.put(0.5, 215);
         monety.put(0.2, 15);
         monety.put(0.1, 135);
         
-        LaczenieZPlikami.zapisz(monety);
-       */
+        MonetyWPamieci.zapisz(monety);
+       
         
         monety= MonetyWPamieci.wczytaj();
         System.out.println(monety.get(0.5));
@@ -30,7 +30,7 @@ public class BiletomatIKasownik {
         // TESTY DO KARTY W PAMIECI I UZYTKOWNICY W PAMIECI
         
         
-        
+        ZarzadzaniePieniedzmi.wydawankoResztki(1.8,5.0);
         
         
        
