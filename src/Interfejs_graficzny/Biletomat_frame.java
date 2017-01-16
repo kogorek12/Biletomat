42df2a8d0ba53df079ed6495c80b5c7448205cb1
Agnieszka <Wojtek@Agnieszka>
2017-01-16 15:55:12 +0100

    Wygląd menu głównego - małe zmiany

--- src/Interfejs_graficzny/Biletomat_frame.java
+++ src/Interfejs_graficzny/Biletomat_frame.java
@@ -12,11 +12,11 @@ public class Biletomat_frame extends JFrame{
     public Biletomat_frame(String s){
     super(s);
     this.setSize(new Dimension(900,600));
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setVisible(true);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
     this.setLocation(233, 84);
     this.setResizable(false);
     this.add(new Panel());
+    this.setVisible(true);
 }
     
     
