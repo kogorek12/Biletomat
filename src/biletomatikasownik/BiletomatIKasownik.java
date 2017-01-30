68190aea786f262f6ef24d9c18882b64b36d8112
Michael <Michael@Computer>
2017-01-30 09:23:40 +0100

    modyfikacja metody main - wywołanie klasy UstawFabrycznie

--- src/biletomatikasownik/BiletomatIKasownik.java
+++ src/biletomatikasownik/BiletomatIKasownik.java
@@ -12,9 +12,14 @@ import pamiec.UzytkownicyWPamieci;
 public class BiletomatIKasownik {
 
     public static void main(String[] args) {
+        
+        UstawFabrycznie ustawianie_poczatkowe = new UstawFabrycznie();
+        ustawianie_poczatkowe.ustawUzytkownikow();
+        ustawianie_poczatkowe.ustawMonetyDoBiletomatu();
+        ustawianie_poczatkowe.ustawKarty();
 
         new Biletomat_frame(Biletomat_frame.NAZWA_FRAME);
-
+        
         HashMap<Integer, Integer> monety = new HashMap();
         monety.put(500, 1000);
         monety.put(200, 100);
@@ -22,7 +27,7 @@ public class BiletomatIKasownik {
         monety.put(50, 100);
         monety.put(20, 100);
         monety.put(10, 100);
-
+        
         MonetyWPamieci.zapisz(monety);
         /*
          monety= MonetyWPamieci.wczytaj();
@@ -41,14 +46,15 @@ public class BiletomatIKasownik {
         //MonetyWPamieci.zapisz(zarzadzaniePieniedzmi.wydawankoResztki(10, 1000));
 
         // MonetyWPamieci.zapisz(zarzadzaniePieniedzmi.przyjmowaniePieniedzy(500));
-        HashMap<Long, Osoba> uzytkownicy = UzytkownicyWPamieci.wczytaj();
-        HashMap<Long, Karta> karty = new HashMap();
-        Osoba uzytkownik = uzytkownicy.get(Long.parseLong("72040615261"));
-        BiletNaKarcie bilet = new BiletNaKarcie(RodzajZnizki.Normalny, BiletKartaTyp.Tygodniowy, new Date());
-        Karta karta = new Karta(uzytkownik, bilet);
-        karty.put(karta.getId(), karta);
-        System.out.println(karta.getBilet().getDataDo());
-        KartyWPamieci.zapisz(karty);
+        
+//        HashMap<Long, Osoba> uzytkownicy = UzytkownicyWPamieci.wczytaj();
+//        HashMap<Long, Karta> karty = new HashMap();
+//        Osoba uzytkownik = uzytkownicy.get(Long.parseLong("72040615261"));
+//        BiletNaKarcie bilet = new BiletNaKarcie(RodzajZnizki.Normalny, BiletKartaTyp.Tygodniowy, new Date());
+//        Karta karta = new Karta(uzytkownik, bilet);
+//        karty.put(karta.getId(), karta);
+//        System.out.println(karta.getBilet().getDataDo());
+//        KartyWPamieci.zapisz(karty);
 
     }
 
